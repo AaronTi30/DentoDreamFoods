b7c6c6ce8a0dd82c072a3aa0c8e9ef8a9b33fee1
Jose Teodoro <joseateodoro.jt@gmail.com>
2024-05-15 20:28:47 -0400

    2024-05-15: updated homepage and added more images to image file

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 import React from "react";
-import * as classes from "./style.module.css";
+import * as classes from "./homepage.module.css";
 
-export const StorePage = () => {
+export default function StorePage() {
   return (
     <div className={classes["STORE-PAGE"]}>
       <div className={classes.div}>
         <div className={classes.element}>
-          <img className={classes.screenshot} alt="Screenshot" src="screenshot-394-1.png" />
+          <img className={classes.screenshot} alt="8oz" src="./components/images/Screenshot_394.png" />
           <div className={classes.text}>
             <div className={classes["text-wrapper"]}>“8oz”</div>
             <div className={classes["text-wrapper-2"]}>Signature Garlic Chili Oil</div>
@@ -19,13 +19,13 @@ export const StorePage = () => {
               <div className={classes["text-wrapper-2"]}>“16oz”</div>
               <div className={classes["text-wrapper-3"]}>Signature Garlic Chili Oil</div>
             </div>
-            <img className={classes.img} alt="Screenshot" src="screenshot-395-1.png" />
+            <img className={classes.img} alt="16oz" src="./components/images/Screenshot_395.png" />
             <div className={classes["text-wrapper-4"]}>$30.00</div>
           </div>
         </div>
         <div className={classes["overlap-wrapper"]}>
           <div className={classes["overlap-group"]}>
-            <img className={classes["screenshot-2"]} alt="Screenshot" src="screenshot-396-1.png" />
+            <img className={classes["screenshot-2"]} alt="32oz" src="./components/images/Screenshot_396.png" />
             <div className={classes.text-2}>
               <div className={classes["text-wrapper-2"]}>“32oz”</div>
               <div className={classes["text-wrapper-3"]}>Signature Garlic Chili Oil</div>
