5544df49824493401f7a0fd881e4b2ea023ee275
Aaron <aaronthakoordeen@gmail.com>
2024-06-21 15:55:25 -0400

    ADD navbar and corresponding links from data.ts file | FIX background color and layout

---
+++ lib/data.ts
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const links = [
+    {
+        name: "Home",
+        hash: "#home",
+    },
+    {
+        name: "About",
+        hash: "#about",
+    },
+    {
+        name: "FAQ",
+        hash: "#faq",
+    },
+    {
+        name: "Contact",
+        hash: "#contact"
+    },
+    {
+        name: "Cart",
+        hash: "#cart",
+    },
+
+] as const;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./components/header";
+import Footer from "./components/footer";
 //import Navbar from './components/navbar/Navbar'
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,14 +13,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-   <html lang="en">
-      <div className="container">
-      <body className={inter.className}>
-        {/* <Navbar /> */}
+   <html lang="en" className="!scroll-smooth">
+      <body className={`${inter.className}bg-gray-50 text-gray-950 pt-28 sm:pt-36`}>
+        <Header />
         {children}
-        <br />
+        <Footer />
         </body>
-        </div>
     </html>
   );
 }
